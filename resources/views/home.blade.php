de1ddd00c89947df979031db6b237c814c5fec61
ashrain <balkanski@outlook.com>
2015-07-29 06:46:58 +0300

    Coming soon

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>NetShell Graph</title>
+	<link href='http://fonts.googleapis.com/css?family=Indie+Flower' rel='stylesheet' type='text/css'>
+	<style type="text/css">
+		body {
+			font-family: 'Indie Flower', cursive;
+			background: #444;
+		}
+		h1 {
+			cursor: default;
+			text-align: center;
+			padding-top: 20vh;
+			font-size: 20vh;
+			color: #FFF;
+		}
+	</style>
+</head>
+<body>
+	<h1>Connect.</h1>
+</body>
+</html>
